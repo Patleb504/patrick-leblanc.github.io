c4355546b3dbc885569699c029b32ffc23c6bf76
Patrick LeBlanc <develop.trick@gmail.com>
2023-03-11 17:34:26 +0000

    eloq

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -4,15 +4,38 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function triangles(x) { // x = number of rows for output triange
-
+  // int var to rep our #
+  var hash = '#'; 
+  // int while loop to help add our # in rows 
+  while (hash.length <= x){
+    //log on each literation to print #
+    console.log(hash);
+    // add 1 # on each literation
+    hash += '#';
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
+// int for loop to loop through numbrs 
+for (var i = 1; i <= 15; i++){
+
+  // int if chain to log fizz buzz or number to console.
+  // check % 3 and 5 for 1st if
+   if(i % 3 === 0 && i % 5 === 0){
+    console.log('fizzbuzz');
+   } else if (i % 3 === 0){
+    console.log('fizz');
+   } else if (i % 5 === 0){
+    console.log('buzz');
+   }else {
+     console.log(i);
+   }
+ }
 }
+//console.log(fizzBuzz(20)); looks good 
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
